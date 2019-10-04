fb92a53c3bb5699aec0b0887784a8b913d41446d
Hanumanth Abhilash <abhilash.hanumanth@appdynamics.com>
2019-10-04 19:13:25 +0530

    Search Elementin Rotated Sorted Array

---
+++ practise/src/main/java/practise/array/problems/SearchElementInSortedRotatedArray.java
@@ -0,0 +1,49 @@
+package practise.array.problems;
+
+public class SearchElementInSortedRotatedArray {
+
+    public static int search(int[] a, int l, int h, int key) {
+        if (l > h)
+            return -1;
+
+        int mid = (l+h)/2;
+        if(a[mid] == key)
+            return mid;
+
+        /* If arr[l...mid] first subarray is sorted */
+        if (a[l] <= a[mid])
+        {
+             /* As this subarray is sorted, we
+               can quickly check if key lies in
+               half or other half */
+            if (key >= a[l] && key <= a[mid]) {
+                return search(a, l, mid-1, key);
+            }
+
+            /*If key not lies in first half subarray,
+           Divide other half  into two subarrays,
+           such that we can quickly check if key lies
+           in other half */
+            return search(a, mid+1, h, key);
+        }
+
+        if (key >= a[mid] && key <= a[h]) {
+            return search(a, mid+1, h, key);
+        }
+
+        return search(a, mid+1, h, key);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,1,2,3};
+        int n = arr.length;
+        int key = 6;
+
+        int i = search(arr, 0, n-1, key);
+
+        if(i != -1)
+            System.out.println("Key found at Index : " + i);
+        else
+            System.out.println("Key not found");
+    }
+}
