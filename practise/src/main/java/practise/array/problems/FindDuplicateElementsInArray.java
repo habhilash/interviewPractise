66cdde6f6ab43a725706b672e2bc30a853515e21
Hanumanth Abhilash <abhilash.hanumanth@appdynamics.com>
2019-10-04 18:12:11 +0530

    Small change in comments in FindDuplicateElementsInArray

--- practise/src/main/java/practise/array/problems/FindDuplicateElementsInArray.java
+++ practise/src/main/java/practise/array/problems/FindDuplicateElementsInArray.java
@@ -1,7 +1,7 @@
 package practise.array.problems;
 
 // This problem can be solved using negating of arr[arr[i]] elements as well. Refer to ReplaceDuplicatesInArray
-
+// Array should contain only n-1 elements.
 public class FindDuplicateElementsInArray {
 
     public static void findDuplicates(int[] arr, int n) {
@@ -11,13 +11,13 @@ public class FindDuplicateElementsInArray {
         }
 
         for(int i=0; i < n; i++) {
-            if (arr[i]/n > 1)
+            if (arr[i] / n > 1)
                 System.out.println(i + " ");
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,1,2};
+        int[] arr = {1,2,1,2,3,3,4,5};
         int n = arr.length;
 
         System.out.println("Repeating elements in the given array are : ");
